675c0534f2aef6e3120e9f5782d87963d5a803a8
Kudzi <kudzimavuwa@yahoo.com>
2023-11-18 14:08:13 +0200

    third commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
-from testrepo.testrepo_stack import MyPipelineStack
+from testrepo.testrepo_stack import testrepo_stack
 
 app = cdk.App()
-MyPipelineStack(app, "MyPipelineStack",
+testrepo_stack(app, "testrepo_stack",
     env=cdk.Environment(account="490774640821", region="eu-west-1")
 )
 
