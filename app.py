7c13ed86c76e865481b04c2954058c2e4b01099b
Kudzi <kudzimavuwa@yahoo.com>
2023-11-18 14:17:22 +0200

    fifth commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from testrepo.testrepo_stack import TestrepoStack
 
 app = cdk.App()
 TestrepoStack(app, "TestrepoStack",
-    env=cdk.Environment(account="490774640821", region="eu-west-1")
+    env=cdk.Environment(account="490774640821", region="us-east-1")
 )
 
 app.synth()
